3c35c5e5e83bdbd6fe466f43651e5a029167334f
Navkirat singh <132665095+Vanillapapa64@users.noreply.github.com>
2024-11-11 19:53:28 +0530

    better more responsive design

--- frontend2/src/components/FullBlog.tsx
+++ frontend2/src/components/FullBlog.tsx
@@ -5,8 +5,8 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
     return <div>
         <Appbar />
         <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-8 lg:pt-12">
-                <div className="col-span-10 p-4 lg:p-8">
+            <div className="grid grid-cols-12 px-2 lg:px-10 w-full pt-200 max-w-screen-xl pt-8 lg:pt-12">
+                <div className="lg:col-span-10 col-span-9 lg:p-8">
                     <div className="sm:text-2xl lg:text-4xl font-extrabold text-slate-500">
                         {blog.title}
                     </div>
@@ -17,12 +17,12 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                         {blog.content}
                     </div>
                 </div>
-                <div className="col-span-2">
+                <div className="col-span-3 pl-4 lg:col-span-2">
                     <div className="text-slate-600 text-lg text-slate-400">
                         Author
                     </div>
                     <div className="flex w-full">
-                        <div className="pr-4 flex flex-col text-2xl justify-center text-white">
+                        <div className="pr-2 flex  flex-col text-xl justify-center text-white">
                             {blog.author.name || "Anonymous"} 
                         </div>
                         
